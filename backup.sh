730cc285fb1e903b8bf9ce66505498d0b7d4c0eb
niveksan <werbekev@hotmail.com>
2023-11-29 17:05:30 +0100

    include attachments folder in backup

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ find /backups/* -mtime +30 -exec rm {} \;
 
 # create backup filename
 BACKUP_FILE="db.sqlite3_$(date "+%F-%H%M%S")"
+BACKUP_FILE_ATT="attachments_$(date "+%F-%H%M%S")"
 
 # use sqlite3 to create backup (avoids corruption if db write in progress)
 sqlite3 /db.sqlite3 ".backup '/tmp/db.sqlite3'"
@@ -15,6 +16,9 @@ sqlite3 /db.sqlite3 ".backup '/tmp/db.sqlite3'"
 
 # TESTEN - gehe in /tmp und erstelle dann das tar von db.sqlite3 - FUNKTIONIERT
 tar cvzf /backups/${BACKUP_FILE}.tar.gz -C /tmp db.sqlite3
+
+# TESTEN - Verzeichnis Attachments packen und in /backups legen
+tar cvzf /backups/${BACKUP_FILE_ATT}.tar.gz -C /attachments
 
 # cleanup tmp folder
 rm -rf /tmp/*
