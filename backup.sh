d9d96f3c1387fe5bf6d7c12e29f5af5dc4ef26c3
niveksan <werbekev@hotmail.com>
2023-12-05 15:47:48 +0100

    update backup file names

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@
 find /backups/* -mtime +30 -exec rm {} \;
 
 # create backup filename
-BACKUP_FILE="db.sqlite3_$(date "+%F-%H%M%S")"
-BACKUP_FILE_ATT="attachments_$(date "+%F-%H%M%S")"
+BACKUP_FILE="$(date "+%F-%H%M%S")_db.sqlite3"
+BACKUP_FILE_ATT="$(date "+%F-%H%M%S")_attachments"
 
 # use sqlite3 to create backup (avoids corruption if db write in progress)
 sqlite3 /db.sqlite3 ".backup '/tmp/db.sqlite3'"
